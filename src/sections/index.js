74b4c35a69803b3e4caaa0f83c8893812f58455d
Joshua <joshuemeredores@gmail.com>
2026-04-03 23:36:04 +0800

    fix the netlify error

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,7 +1,7 @@
 import Hero from "./Hero";
 import Skills from "./Skills";
 
-import Service from "./service";
+import Service from "./Service";
 import Project from "./Project";
 import Contact from "./Contact";
 
